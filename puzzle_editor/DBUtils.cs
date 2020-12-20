af385753d6d071c21c60fc362e73f27463d5afac
Bogdan <bogdanbondarenko19@yandex.ru>
2020-12-20 15:23:38 +0400

    First commit

---
+++ puzzle_editor/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace puzzle_editor
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "127.0.0.1";
+            int port = 3306;
+            string database = "model";
+            string username = "root";
+            string password = "abc8734";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
---
+++ puzzle_editor/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace puzzle_editor
+{
+    public partial class Form1 : Form
+    {
+        //public NpgsqlConnection connection = new NpgsqlConnection("Host=localhost;Username=root;Password=abc8734;Database=model");
+        public Form1()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
